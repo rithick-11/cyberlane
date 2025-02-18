ba7ab042c522ac8a8e4ddbb103261b21efe32483
rithick-11 <rithickroshan7878@gmail.com>
2025-02-18 21:46:56 +0530

    fix ui

--- src/components/CollegeCard.jsx
+++ src/components/CollegeCard.jsx
@@ -2,7 +2,7 @@
 
 const CollegeCard = () => {
   return (
-    <div className="flex justify-between items-center text-white">
+    <div className="flex flex-col md:flex-row  justify-between items-center text-white">
       <img
         className="h-40 w-auto"
         src="https://res.cloudinary.com/dwpmsw2i4/image/upload/v1739894754/college%20logo.png"
@@ -12,7 +12,7 @@ const CollegeCard = () => {
         <h1 className="font-light text-2xl md:text-3xl text-center">
           Mahendra Engineering College (Autonomous)
         </h1>
-        <p className="font-extralight text-sm md:text-lg w-[80%] text-center">
+        <p className="font-extralight text-xs sm:text-sm md:text-lg w-[80%] text-center">
           Autonomous Institution Approved by AICTE | Recognized U/S 12(B) & 2(F)
           of UGC ACT 1956 Affiliated to Anna University, Chennai | NAAC
           Accredited with 'A++' Grade & NBA Tier - 1(WA) UG: CSE,ECE,EEE
@@ -20,7 +20,7 @@ const CollegeCard = () => {
       </div>
       {/* <img className="h-20 w-auto" src="https://mahendra.org/wp-content/uploads/2021/04/mahendra_text-only-1.png" /> */}
       <img
-        className="h-40 w-auto"
+        className="h-40 w-auto hidden md:block"
         src="https://res.cloudinary.com/dwpmsw2i4/image/upload/v1739894754/college%20logo.png"
         alt="logo 1"
       />
