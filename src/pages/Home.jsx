a4f1c2e299403cb27e8c1f628aac88979d1d8894
rithick-11 <rithickroshan7878@gmail.com>
2025-02-19 19:09:09 +0530

    fit viste count

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,21 @@
+import { useEffect } from "react";
 import BgContainer from "../components/BgContainer";
 import Container from "../components/Container";
 import EventSection from "../components/EventSection";
 import HeroSection from "../components/HeroSection";
 import LocationSection from "../components/LocationSection";
 import NavBar from "../components/NavBar";
+const isDev = import.meta.env.VITE_MODE == "DEV";
 
 const Home = () => {
+  useEffect(() => {
+    if (!isDev) {
+      fetch(
+        "https://portfolio-server-pink-seven.vercel.app/api/cyberlane/vistcount"
+      );
+    }
+  }, []);
+
   return (
     <section>
       <BgContainer />
@@ -17,7 +27,15 @@ const Home = () => {
       </Container>
       <footer className="text-center text-sm text-white/50 py-2 bg-gray-900">
         <p>
-          © 2025 Cyberlane. Developed by <a href="https://rithick-11.github.io/portfolio" target="_blank" className="underline">Rithickroshan S</a>.
+          © 2025 Cyberlane. Developed by{" "}
+          <a
+            href="https://rithick-11.github.io/portfolio"
+            target="_blank"
+            className="underline"
+          >
+            Rithickroshan S
+          </a>
+          .
         </p>
       </footer>
     </section>
