93ece19d83206b2e50f1e628456622c779c7e142
rithick-11 <rithickroshan7878@gmail.com>
2025-02-20 21:52:07 +0530

    fit no

--- src/components/CountCard.jsx
+++ src/components/CountCard.jsx
@@ -13,15 +13,16 @@ const CountCard = () => {
       <div className="p-6 rounded border border-orange-400 bg-gray-800/30 w-fit ">
         {count?.count.count}
       </div>
-      <ul className="max-h-[60vh] w-full md:w-[60vw] border overflow-auto py-2 px-3 mt-4 rounded-2xl  border-amber-400">
+      <ul className="max-h-[60vh] w-full md:w-[60vw] border overflow-y-auto overflow-x-hidden py-2 px-3 mt-4 rounded-2xl  border-amber-400">
         {count?.count.RecentVist.slice()
           .reverse()
-          .map((vist) => (
-            <li key={vist._id} className="grid grid-cols-4">
-              <p>{`${format(vist.date, "dd-MMM-yy hh:mm a")}`}</p>
-              <p>{vist.record?.browser?.split(";")[0]}</p>
-              <p>{vist.record?.platform}</p>
-              <p>{vist.record?.location}</p>
+          .map((vist, i) => (
+            <li key={vist._id} className="grid grid-cols-5 border-b">
+              <p className="text-center">{count?.count.count - i}</p>
+              <p className="text-center">{`${format(vist.date, "dd-MM-yy hh:mm a")}`}</p>
+              <p className="text-center">{vist.record?.browser?.split(";")[0]}</p>
+              <p className="text-center">{vist.record?.platform}</p>
+              <p className="text-center">{vist.record?.location}</p>
             </li>
           ))}
       </ul>
